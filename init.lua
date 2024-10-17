51d5ee0ecdd9cd827f1e1e95265461a86fafb8c9
KwaejenAepul <66973949+KwaejenAepul@users.noreply.github.com>
2024-10-17 15:47:22 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -838,13 +838,14 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     --'folke/tokyonight.nvim',
-    'navarasu/onedark.nvim',
+    --'navarasu/onedark.nvim',
+    'tanvirtin/monokai.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'monokai_pro'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
